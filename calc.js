928277584cf9835386eec4ee252cf9991e36710c
James Hager <james@avantiapplications.com>
2022-01-19 15:59:39 -0700

    Added square, cube, and x^y

--- calc.js
+++ calc.js
@@ -18,20 +18,41 @@ reader.question("What would you like to calculate?", function(input){
 	// console.log(num1)
 	// console.log(num2)
 
-	if (mathSymbol === '+') {
-		console.log(num1, mathSymbol, num2, "=", num1 + num2)
+	switch (mathSymbol) {
+		case '+':
+			console.log(num1, mathSymbol, num2, "=", num1 + num2)
+			break
 
-	} else if ( mathSymbol === '-') {
-		console.log(num1, mathSymbol, num2, "=", num1 - num2)
+		case '-':
+			console.log(num1, mathSymbol, num2, "=", num1 - num2)
+			break
 
-	} else if ( mathSymbol === '*') {
-		console.log(num1, mathSymbol, num2, "=", num1 * num2)
+		case '*':
+			console.log(num1, mathSymbol, num2, "=", num1 * num2)
+			break
 
-	} else if ( mathSymbol === '/') {
-		console.log(num1, mathSymbol, num2, "=", num1 / num2)
+		case '/':
+			console.log(num1, mathSymbol, num2, "=", num1 / num2)
+			break
 
-	} else if ( mathSymbol === 'sqrt') {
-		console.log(`sqrt(${num1}) =`, Math.sqrt(num1))
+		case 'sqrt':
+			console.log(`sqrt(${num1}) =`, Math.sqrt(num1))
+			break
+
+		case 'square':
+			console.log(`${num1}^2 =`, num1 * num1)
+			break
+
+		case 'cube':
+			console.log(`${num1}^3 =`, num1 * num1 * num1)
+			break
+
+		case 'x^y':
+			console.log(`${num1}^${num2} =`, Math.pow(num1,num2))
+			break
+
+		default:
+			console.log(`The mathSymbol "${mathSymbol}" is not supported`)
 	}
 
 	// This line closes the connection to the command line interface.
